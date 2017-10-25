f8bc76024bf6117d7a1456963fc0f5a644464caa
Nicholas Land <land.d.nicholas@gmail.com>
2017-10-25 14:48:50 -0400

    Refactored testing class name

--- app/src/androidTest/kotlin/me/nickland/inspirationalquote/InspirationalQuoteInstrumentedTest.kt
+++ app/src/androidTest/kotlin/me/nickland/inspirationalquote/InspirationalQuoteInstrumentedTest.kt
@@ -20,7 +20,7 @@ import org.junit.runner.RunWith
  */
 @RunWith(AndroidJUnit4::class)
 @LargeTest
-class ExampleInstrumentedTest {
+class InspirationalQuoteInstrumentedTest {
 
     @Rule
     @JvmField
