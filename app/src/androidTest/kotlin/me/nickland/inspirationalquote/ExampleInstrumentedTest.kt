64e0ad4d49f8c3de78553d8badea4dc9f997565a
Nicholas Land <land.d.nicholas@gmail.com>
2017-10-25 14:34:19 -0400

    Add first working acceptance test
    This isn't exactly the greatest accomplishment, but it's a working implementation of an acceptance test.

---
+++ app/src/androidTest/kotlin/me/nickland/inspirationalquote/ExampleInstrumentedTest.kt
@@ -0,0 +1,42 @@
+package me.nickland.inspirationalquote
+
+import android.support.test.InstrumentationRegistry
+import android.support.test.espresso.Espresso.onView
+import android.support.test.espresso.assertion.ViewAssertions.matches
+import android.support.test.espresso.matcher.ViewMatchers.withId
+import android.support.test.espresso.matcher.ViewMatchers.withText
+import android.support.test.filters.LargeTest
+import android.support.test.rule.ActivityTestRule
+import android.support.test.runner.AndroidJUnit4
+import org.junit.Assert.assertEquals
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+/**
+ * Instrumentation test, which will execute on an Android device.
+ *
+ * @see [Testing documentation](http://d.android.com/tools/testing)
+ */
+@RunWith(AndroidJUnit4::class)
+@LargeTest
+class ExampleInstrumentedTest {
+
+    @Rule
+    @JvmField
+    val mActivityRule: ActivityTestRule<InspirationalQuoteActivity> = ActivityTestRule(InspirationalQuoteActivity::class.java)
+
+    @Test
+    @Throws(Exception::class)
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getTargetContext()
+        assertEquals("me.nickland.inspirationalquote", appContext.packageName)
+    }
+
+    @Test
+    fun ensureTheQuoteOfTheDayIsDisplayed() {
+        onView(withId(R.id.inspirationalQuote))
+                .check(matches(withText("A brand new inspirational quote")))
+    }
+}
--- app/src/test/kotlin/me/nickland/inspirationalquote/InspirationalQuoteActivityUnitTest.kt
+++ app/src/test/kotlin/me/nickland/inspirationalquote/InspirationalQuoteActivityUnitTest.kt
@@ -1,19 +1,19 @@
 package me.nickland.inspirationalquote
 
+import org.junit.Assert.assertNotNull
 import org.junit.Test
-
-import org.junit.Assert.assertEquals
 
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
  * @see [Testing documentation](http://d.android.com/tools/testing)
  */
-class ExampleUnitTest {
+class InspirationalQuoteActivityUnitTest {
 
     @Test
     @Throws(Exception::class)
-    fun assert() {
-        assertEquals(4, (2 + 2).toLong())
+    fun assertInspirationalQuoteActivityNotNull() {
+        val inspirationalQuote = InspirationalQuoteActivity()
+        assertNotNull(inspirationalQuote)
     }
 }
